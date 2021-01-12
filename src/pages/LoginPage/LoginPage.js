0fbc4049ba8ea81aa7189ef06b88a9c706fe8097
rev bar <revital.jer@gmail.com>
2021-01-12 17:33:46 +0200

    addind inputs loginPage as controled components+ basic layout

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
 import './LoginPage.css';
 
 function LoginPage(props) {
+
+    // const [email, setEmail]= useState("revital@revital.com")
+    // const [pwd, setPwd]= useState(1234)
+
+    const [email, setEmail]= useState("");
+    const [pwd, setPwd]= useState("");
+
     return (
-        <div>
-            <p>LoginPage</p>
-        </div>
+        <Container>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" value ={email} onChange={e=> setEmail (e.target.value)} placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" value ={pwd} onChange={e=> setPwd (e.target.value)} placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="button">
+                    Submit
+                </Button>
+            </Form>
+        </Container>
+
     );
 }
 
