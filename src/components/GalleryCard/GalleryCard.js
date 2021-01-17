d6483e822ae2caaa2c1adc57d48f77fd4fbdc457
rev bar <revital.jer@gmail.com>
2021-01-17 20:49:26 +0200

    rendering gallery page by id from galleries page

--- src/components/GalleryCard/GalleryCard.js
+++ src/components/GalleryCard/GalleryCard.js
@@ -1,18 +1,40 @@
-import { Card } from 'react-bootstrap';
+import { useEffect ,useState } from 'react';
+import { Card, Button } from 'react-bootstrap';
 import './GalleryCard.css';
 
 function GalleryCard(props) {
 
     const {gallery, artworks} = props;
+    // const [filterdArtwork, setFilterdArtwork]= useState([])
+    // // console.log(artworks[0].galleryId);
+    
+    //   // console.log(artworks.length);
+    // useEffect( ()=>{
+    //     for (let i=0 ; i< artworks.length ; i++){
+    //     // console.log(artworks);
+    //     // console.log(gallery);
+    //     console.log(artworks[0]);
 
+    //         if  (gallery.id=== artworks[i].galleryId){
+    //             setFilterdArtwork(filterdArtwork.concat(artworks[i])) 
+    //         }
+    //     }
+        
+    // },[artworks])   
+    // debugger;
+   if (artworks !== []){
+    console.log(artworks[0]);
+   }
+   const buttonLink= "#/ArtistGalleries/"+ gallery.name
+    
     return (
         <div className="c-gallery-card">
         <Card>
             <Card.Body>
                 <Card.Title>{gallery.name}</Card.Title>
             </Card.Body>
-            <Card.Img variant="bottom" src={artworks[0].img} />
-            <Button variant="primary">Go to {gallery.name} </Button>
+            {/* <Card.Img variant="bottom" src={artworks[0].img} /> */}
+            <Button href={buttonLink} variant="primary" >Go to {gallery.name} </Button>
         </Card>
 
     </div>
