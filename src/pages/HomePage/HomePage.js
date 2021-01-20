cfc1dff9483ce7269f9bcc71b15c0d92ac8b5e30
rev bar <revital.jer@gmail.com>
2021-01-20 11:20:37 +0200

    handeling homePage logout

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import { useContext } from 'react';
 import KidsGalleryNavBar from '../../components/KidsGalleryNavBar/KidsGalleryNavBar';
+import ActiveUserContext from '../../shared/ActiveUserContext';
 import './HomePage.css';
 
 function HomePage(props) {
+    const {onLogout} = props;
+    const activeUser= useContext(ActiveUserContext);
+
     return (
         <div>
-            <KidsGalleryNavBar></KidsGalleryNavBar>
+            <KidsGalleryNavBar onLogout={onLogout}></KidsGalleryNavBar>
             <p>HomePage</p>
         </div>
     );
