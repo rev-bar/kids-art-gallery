acf6a6d0b43272f30d6e1232e8063651ef673aa5
rev bar <revital.jer@gmail.com>
2021-01-20 20:04:17 +0200

    rendering GalleryOwnerArtistsPage+ cards

---
+++ src/components/ArtistCard/ArtistCard.js
@@ -0,0 +1,24 @@
+
+import { Card, Button } from 'react-bootstrap';
+import './ArtistCard.css';
+
+function ArtistCard(props) {
+
+    const {artist} = props;
+
+    console.log(artist);
+
+    return (
+        <div className="c-artist-card">
+            <Card>
+            <Card.Body>                 
+                {artist? <Card.Title className="name"> {artist.username}</Card.Title > : null }
+                {artist? <Card.Title className="about"> {artist.about}</Card.Title> : null }
+            </Card.Body>
+            <Button href= "#" variant="primary" > Edit </Button>
+        </Card>
+        </div>
+    );
+}
+
+export default ArtistCard;
--- src/pages/GalleryOwnerArtistsPage/GalleryOwnerArtistsPage.js
+++ src/pages/GalleryOwnerArtistsPage/GalleryOwnerArtistsPage.js
@@ -1,13 +1,71 @@
-
+import { useContext } from 'react';
 import KidsGalleryNavBar from '../../components/KidsGalleryNavBar/KidsGalleryNavBar';
+import ActiveUserContext from '../../shared/ActiveUserContext';
+import { Redirect } from 'react-router-dom';
+import GalleryModel from '../../model/GalleryModel';
+import { useEffect,useState } from 'react';
+import Parse from 'parse';
+import { Col, Container, Row ,Button , Image } from 'react-bootstrap';
+import OwnerGalleryCard from '../../components/OwnerGalleryCard/OwnerGalleryCard';
+import UserModel from '../../model/UserModel';
+import NewGalleryModal from '../../components/NewGalleryModal/NewGalleryModal';
+import './GalleryOwnerArtistsPage.css';
+import ArtistCard from '../../components/ArtistCard/ArtistCard';
+
 
 function GalleryOwnerArtistsPage(props) {
     const {onLogout} = props;
+    const activeUser= useContext(ActiveUserContext);
+    const [artists, setArtists]= useState([])
+    const [showModal, setShowModal] = useState(false);
+ 
+    useEffect( ()=>{
+     
+        async function fetchData() {
+        try {
+            
+            const User = new Parse.User();
+            const ArtistQuery = new Parse.Query(User);
+            ArtistQuery.equalTo("role","artist");
+
+            const artistsData = await ArtistQuery.find(); 
+            console.log('Artists found', artistsData);
+            const fetchedArtists=artistsData.map(artist=> new UserModel(artist)) ; 
+                  
+            setArtists(fetchedArtists);
+            
+        }  catch(error) {
+            // show an error alert
+            console.error('Error while fetching data', error);
+        }
+        }
+
+        if (activeUser){
+            fetchData()
+        
+        }
+   
+    },[activeUser])
+
+ 
+    if (!activeUser) {
+        return <Redirect to="#/"/>
+    }
+
+    const artistsView = artists.map(artist => <Col key={artist.id} xl={3}  md={4}><ArtistCard artist= {artist}  /></Col>)
 
     return (
-        <div>
+        <div className="p-GalleryOwnerArtistsPage">
             <KidsGalleryNavBar onLogout={onLogout}></KidsGalleryNavBar>
-            <p>GalleryOwnerArtistsPage</p>
+            <p>{activeUser.username}'s artists</p>
+            <Container>
+                <Row>
+                    <Button variant="info" onClick={() => setShowModal(true)} >
+                        <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFW8Goxnhxje8kvnJ4EzvnxpybClc1oiM4nQ&usqp=CAU"/>
+                    </Button> 
+                    {artistsView}
+                </Row>
+            </Container>
         </div>
     );
 }
